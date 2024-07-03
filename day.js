958dd3058de92ef8fec719b983b19a4db0b4f9bb
Anik Chowdhury <acdevilliersanik17@gmail.com>
2024-07-03 04:07:32 -0700

    add a file

---
+++ day.js
@@ -0,0 +1,14 @@
+var date =new Date();
+console.log(date);
+
+var year = date.getDate();
+console.log(year);
+
+var month =date.getMonth();
+console.log(month);
+var currentday = date.getDay();
+console.log(currentday);
+var hour =date.getHours();
+console.log(hour);
+
+   
